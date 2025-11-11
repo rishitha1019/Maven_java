b9f6265f2007d67a7b6a3e93e3cd50a9db602b50
rishitha1019 <19rishitha@gmail.com>
2025-11-11 12:08:44 +0530

    first commit

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/WebRev/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2><%= "Hello hi World!" %></h2>
+</body>
+</html>
---
+++ RishithaJava/src/main/java/SEGroup/RishithaJava/App.java
@@ -0,0 +1,13 @@
+package SEGroup.RishithaJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Rishitha!" );
+    }
+}
